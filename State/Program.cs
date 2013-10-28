5978e8b636e9f4c7430a58fb3cdf5a4045216046
cashwu <cash@cashwu.com>
2013-10-29 00:16:01 +0800

    FirstCommit

---
+++ State/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Agent
+    {
+        public static readonly int HEALTH_LOW_LIMIT = 70;
+        public static readonly int INJURED_LOW_LIMIT = 1;
+        public static readonly int DEAD_LOW_LIMIT = 0;
+
+        private static int MAX_HP = 100;
+
+        private IAgentState _currentState = null;
+
+        private int _HP = 0;
+
+        public Agent()
+        {
+            _currentState = new Health();
+            _HP = MAX_HP;
+        }
+
+        public int getHP()
+        {
+            return _HP;
+        }
+
+        public IAgentState getState()
+        {
+            return _currentState;
+        }
+
+        public void search()
+        {
+            _currentState.search(this);
+        }
+
+        public void filgth()
+        {
+            _currentState.fight(this);
+        }
+
+        public void hit(int value)
+        {
+            _HP = _HP - value;
+            _currentState.Hit(this);
+        }
+
+        public void heal(int value)
+        {
+            if (_HP < MAX_HP)
+            {
+                _HP = _HP + value;
+            }
+
+            _currentState.Heal(this);
+        }
+
+        public void changeState(IAgentState state)
+        {
+            _currentState = state;
+        }
+    }
+
+    public interface IAgentState
+    {
+        void search(Agent agent);
+
+        void fight(Agent agent);
+
+        void Hit(Agent agent);
+
+        void Heal(Agent agent);
+    }
+
+    public class Health : IAgentState
+    {
+        public void search(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void fight(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hit(Agent agent)
+        {
+            if (agent.getHP() < Agent.INJURED_LOW_LIMIT)
+            {
+                agent.changeState(new Dead());
+            }
+            else if (agent.getHP() < Agent.HEALTH_LOW_LIMIT)
+            {
+                agent.changeState(new Injured());
+            }
+        }
+
+        public void Heal(Agent agent)
+        {
+            if (agent.getHP() >= Agent.HEALTH_LOW_LIMIT)
+            {
+                agent.changeState(new Health());
+            }
+            else if (agent.getHP() >= Agent.INJURED_LOW_LIMIT)
+            {
+                agent.changeState(new Injured());
+            }
+        }
+    }
+
+    public class Injured : IAgentState
+    {
+        public void search(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void fight(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hit(Agent agent)
+        {
+            if (agent.getHP() < Agent.INJURED_LOW_LIMIT)
+            {
+                agent.changeState(new Dead());
+            }
+        }
+
+        public void Heal(Agent agent)
+        {
+            if (agent.getHP() >= Agent.HEALTH_LOW_LIMIT)
+            {
+                agent.changeState(new Health());
+            }
+        }
+    }
+
+    public class Dead : IAgentState
+    {
+        public void search(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void fight(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hit(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Heal(Agent agent)
+        {
+            if (agent.getHP() >= Agent.HEALTH_LOW_LIMIT)
+            {
+                agent.changeState(new Health());
+            }
+            else if (agent.getHP() >= Agent.INJURED_LOW_LIMIT)
+            {
+                agent.changeState(new Injured());
+            }
+        }
+    }
+}
---
+++ StateTests1/AgentTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using State;
+
+namespace State.Tests
+{
+    [TestClass()]
+    public class AgentTests
+    {
+        [TestMethod()]
+        public void AgentTest()
+        {
+            Agent agent = new Agent();
+            Assert.IsNotNull(agent);
+
+            Assert.AreEqual(typeof(Health), agent.getState().GetType());
+
+            agent.heal(80);
+            Assert.AreEqual(100, agent.getHP());
+            Assert.AreEqual(typeof(Health), agent.getState().GetType());
+
+            agent.hit(30);
+            Assert.AreEqual(typeof(Health), agent.getState().GetType());
+            agent.hit(1);
+            Assert.AreEqual(typeof(Injured), agent.getState().GetType());
+
+            agent.heal(31);
+            Assert.AreEqual(100, agent.getHP());
+            Assert.AreEqual(typeof(Health), agent.getState().GetType());
+
+            agent.hit(100);
+            Assert.AreEqual(0, agent.getHP());
+            Assert.AreEqual(typeof(Dead), agent.getState().GetType());
+        }
+
+        //[TestMethod()]
+        //public void getHPTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void getStateTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void searchTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void filgthTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void hitTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void healTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void changeStateTest()
+        //{
+        //    Assert.Fail();
+        //}
+    }
+}
